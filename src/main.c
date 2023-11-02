c083c505bff362aea582d5f2d4af5b722b41aa1b
Lucaseggi <lseggiaro@itba.edu.ar>
2023-11-02 12:44:11 -0300

    Clase 1: Parches y Pop3cmd

---
+++ src/main.c
@@ -0,0 +1,228 @@
+/**
+ * main.c - servidor proxy socks concurrente
+ *
+ * Interpreta los argumentos de línea de comandos, y monta un socket
+ * pasivo. Por cada nueva conexión lanza un hilo que procesará de
+ * forma bloqueante utilizando el protocolo SOCKS5.
+ */
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <errno.h>
+#include <pthread.h>
+#include <signal.h>
+#include <stdbool.h>
+
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+#include <unistd.h>
+#include "buffer.h"
+#include "netutils.h"
+#include "tests.h"
+#include "pop3cmd.h"
+
+static bool done = false;
+
+static void sigterm_handler(const int signal) {
+    printf("signal %d, cleaning up and exiting\n",signal);
+    done = true;
+}
+
+/**
+ * estructura utilizada para transportar datos entre el hilo
+ * que acepta sockets y los hilos que procesa cada conexión
+ */
+
+struct connection {
+  int fd; 
+  socklen_t addrlen;
+  struct sockaddr_in6 addr;
+};
+
+
+// #include "socks5.h
+
+/**
+ * maneja cada conexión entrante
+ *
+ * @param fd   descriptor de la conexión entrante.
+ * @param caddr información de la conexiónentrante.
+ */
+
+static void pop3_handle_connection(const int fd, const struct sockaddr *caddr) {
+    struct buffer serverBuf;
+    buffer *sb = &serverBuf;
+    uint8_t serverDirectBuff[1024];
+    buffer_init(&serverBuf, N(serverDirectBuff), serverDirectBuff);
+    
+    struct buffer clientBuf;
+    buffer *cb = &clientBuf;
+    uint8_t clientDirectBuff[1024];
+    buffer_init(&clientBuf, N(clientDirectBuff), clientDirectBuff);
+
+
+    // enviar saludo
+    memcpy(serverDirectBuff, "+OK POP3 server ready\r\n", 23);
+    buffer_write_adv(sb, 23);
+    sock_blocking_write(fd, cb);
+
+    // leemos comando
+    {
+        bool error = false;
+        size_t buffsize;
+        ssize_t n;
+        struct pop3cmd_parser pop3cmd_parser = { 
+        };
+        pop3cmd_parser_init(&pop3cmd_parser);
+
+        
+        do {
+            uint8_t *ptr = buffer_write_ptr(&clientBuf, &buffsize);
+            n = recv(fd, ptr, buffsize, 0); 
+            if(n > 0) {
+                buffer_write_adv(&clientBuf, n); 
+                const enum pop3cmd_state st = pop3cmd_consume(&clientBuf, &pop3cmd_parser, &error);
+                if(pop3cmd_is_done(st, &error)) {
+                    break;
+                }
+
+            } else {
+                break;
+            }   
+        } while(true);
+        // if(!pop3cmd_is_done(pop3cmd_parser.state, &error)) {
+        //     error = true;
+        // }
+        // pop3cmd_parser_close(&pop3cmd_parser);
+        // return error;
+    }
+
+
+
+    close(fd);
+}
+
+/** rutina de cada hilo worker */
+
+static void *handle_connection_pthread(void *args) {
+  const struct connection *c = args;
+  pthread_detach(pthread_self());
+  pop3_handle_connection(c->fd, (struct sockaddr *)&c->addr);
+  free(args);
+  return 0;
+}
+
+
+
+ /**
+ * atiende a los clientes de forma concurrente con I/O bloqueante.
+ */
+int serve_pop3_concurrent_blocking(const int server) {
+  for (;!done;) {
+    struct sockaddr_in6 caddr;
+    socklen_t caddrlen = sizeof (caddr);
+    // Wait for a client to connect
+    const int client = accept(server, (struct sockaddr*)&caddr, &caddrlen);
+    if (client < 0) {
+      perror("unable to accept incoming socket");
+    } else {
+      // TODO(juan): limitar la cantidad de hilos concurrentes
+      struct connection* c = malloc(sizeof (struct connection));
+      if (c == NULL) {
+        // lo trabajamos iterativamente
+        pop3_handle_connection(client, (struct sockaddr*)&caddr);
+      } else {
+        pthread_t tid;
+        c->fd = client;
+        c->addrlen = caddrlen;
+        memcpy(&(c->addr), &caddr, caddrlen);
+        if (pthread_create(&tid, 0, handle_connection_pthread, c)) {
+          free(c);
+          // lo trabajamos iterativamente
+          pop3_handle_connection(client, (struct sockaddr*)&caddr);
+        }
+      }
+    }
+  }  
+  return 0;
+}
+
+
+
+
+
+
+int
+main(const int argc, const char **argv) {
+    unsigned port = 1110;
+
+    if(argc == 1) {
+        // utilizamos el default
+    } else if(argc == 2) {
+        char *end     = 0;
+        const long sl = strtol(argv[1], &end, 10);
+
+        if (end == argv[1]|| '\0' != *end 
+           || ((LONG_MIN == sl || LONG_MAX == sl) && ERANGE == errno)
+           || sl < 0 || sl > USHRT_MAX) {
+            fprintf(stderr, "port should be an integer: %s\n", argv[1]);
+            return 1;
+        }
+        port = sl;
+    } else {
+        fprintf(stderr, "Usage: %s <port>\n", argv[0]);
+        return 1;
+    }
+    struct sockaddr_in addr;
+    memset(&addr, 0, sizeof(addr));
+    addr.sin_family      = AF_INET;
+    addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    addr.sin_port        = htons(port);
+
+    const char *err_msg;
+
+    const int server = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if(server < 0) {
+        err_msg = "unable to create socket";
+        goto finally;
+    }
+
+    fprintf(stdout, "Listening on TCP port %d\n", port);
+
+    // man 7 ip. no importa reportar nada si falla.
+    setsockopt(server, SOL_SOCKET, SO_REUSEADDR, &(int){ 1 }, sizeof(int));
+
+    if(bind(server, (struct sockaddr*) &addr, sizeof(addr)) < 0) {
+        err_msg = "unable to bind socket";
+        goto finally;
+    }
+
+    if (listen(server, 20) < 0) {
+        err_msg = "unable to listen";
+        goto finally;
+    }
+    
+    // registrar sigterm es útil para terminar el programa normalmente.
+    // esto ayuda mucho en herramientas como valgrind.
+    signal(SIGTERM, sigterm_handler);
+    signal(SIGINT,  sigterm_handler);
+
+    
+
+    err_msg = 0;
+    int ret = serve_pop3_concurrent_blocking(server);
+
+    
+
+finally:
+    if(err_msg) {
+        perror(err_msg);
+        ret = 1;
+    }
+    if(server >= 0) {
+        close(server);
+    }
+    return ret;
+}
--- src/pop3cmd.h
+++ src/pop3cmd.h
@@ -1,5 +1,5 @@
-#ifndef HELLO_H_Ds3wbvgeUHWkGm7B7QLXvXKoxlA
-#define HELLO_H_Ds3wbvgeUHWkGm7B7QLXvXKoxlA
+#ifndef pop3cmd_H_Ds3wbvgeUHWkGm7B7QLXvXKoxlA
+#define pop3cmd_H_Ds3wbvgeUHWkGm7B7QLXvXKoxlA
 
 #include <stdint.h>
 #include <stdbool.h>
@@ -26,71 +26,71 @@ static const uint8_t METHOD_NO_ACCEPTABLE_METHODS      = 0xFF;
 /* no es un ADT/CDT para no evitar malloc/free */
 /** estado del parser de hello request */
 
-enum hello_state {
-    hello_version,
+enum pop3cmd_state {
+    pop3cmd_version,
     /** debemos leer la cantidad de metodos */
-    hello_nmethods,
+    pop3cmd_nmethods,
     /** nos encontramos leyendo los métodos */
-    hello_methods,
-    hello_done,
-    hello_error_unsupported_version,
+    pop3cmd_methods,
+    pop3cmd_done,
+    pop3cmd_error_unsupported_version,
 };
 
-struct hello_parser {
+struct pop3cmd_parser {
     /** invocado cada vez que se presenta un nuevo método */
     void (*on_authentication_method) 
-         (struct hello_parser *parser, const uint8_t method);
+         (struct pop3cmd_parser *parser, const uint8_t method);
 
     /** permite al usuario del parser almacenar sus datos */
     void *data;
     /******** zona privada *****************/
-    enum hello_state state;
+    enum pop3cmd_state state;
     /* metodos que faltan por leer */
     uint8_t remaining;
 };
 
 /** inicializa el parser */
-void hello_parser_init (struct hello_parser *p);
+void pop3cmd_parser_init (struct pop3cmd_parser *p);
 
 /** entrega un byte al parser. retorna true si se llego al final  */
-enum hello_state hello_parser_feed (struct hello_parser *p, uint8_t b);
+enum pop3cmd_state pop3cmd_parser_feed (struct pop3cmd_parser *p, uint8_t b);
 
 /**
- * por cada elemento del buffer llama a `hello_parser_feed' hasta que
+ * por cada elemento del buffer llama a `pop3cmd_parser_feed' hasta que
  * el parseo se encuentra completo o se requieren mas bytes.
  *
  * @param errored parametro de salida. si es diferente de NULL se deja dicho
  *   si el parsing se debió a una condición de error
  */
-enum hello_state
-hello_consume(buffer *b, struct hello_parser *p, bool *errored);
+enum pop3cmd_state
+pop3cmd_consume(buffer *b, struct pop3cmd_parser *p, bool *errored);
 
 /**
- * Permite distinguir a quien usa hello_parser_feed si debe seguir
+ * Permite distinguir a quien usa pop3cmd_parser_feed si debe seguir
  * enviando caracters o no. 
  *
  * En caso de haber terminado permite tambien saber si se debe a un error
  */
 bool 
-hello_is_done(const enum hello_state state, bool *errored);
+pop3cmd_is_done(const enum pop3cmd_state state, bool *errored);
 
 /**
  * En caso de que se haya llegado a un estado de error, permite obtener una
  * representación textual que describe el problema
  */
 extern const char *
-hello_error(const struct hello_parser *p);
+pop3cmd_error(const struct pop3cmd_parser *p);
 
 
 /** libera recursos internos del parser */
-void hello_parser_close(struct hello_parser *p);
+void pop3cmd_parser_close(struct pop3cmd_parser *p);
 
-static const uint8_t SOCKS_HELLO_NOAUTHENTICATION_REQUIRED = 0x00;
+static const uint8_t SOCKS_pop3cmd_NOAUTHENTICATION_REQUIRED = 0x00;
 /*
  * If the selected METHOD is X'FF', none of the methods listed by the
    client are acceptable, and the client MUST close the connection.
  */
-static const uint8_t SOCKS_HELLO_NO_ACCEPTABLE_METHODS = 0xFF;
+static const uint8_t SOCKS_pop3cmd_NO_ACCEPTABLE_METHODS = 0xFF;
 
 
 /**
@@ -100,6 +100,6 @@ static const uint8_t SOCKS_HELLO_NO_ACCEPTABLE_METHODS = 0xFF;
  * espacio suficiente.
  */
 int
-hello_marshall(buffer *b, const uint8_t method);
+pop3cmd_marshall(buffer *b, const uint8_t method);
 
 #endif
